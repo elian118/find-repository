bcdc038f71309b22e24431cbcad488e81dac8f7b
elian <elian118@naver.com>
2025-03-19 21:01:21 +0900

    불필요 설정 제거

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    extends: ['next/core-web-vitals', 'next/typescript'],
     rules: {
       '@typescript-eslint/interface-name-prefix': 'on',
       '@typescript-eslint/explicit-function-return-type': 'on',
